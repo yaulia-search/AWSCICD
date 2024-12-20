136b4668e7daf2632ab58b3e34c894f229a16c90
Admin <admin@Entrys-MacBook-Air.local>
2024-12-20 17:04:12 +0700

    add pop.txt

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeploystack-github-webappdeploymentbucket-hxoy7zpcsewb/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://codedeploystack-githubv1-webappdeploymentbucket-dmsdtttnatzr/SpringBootHelloWorldExampleApplication.war/usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
 
 # Ensure the ownership permissions are correct.
